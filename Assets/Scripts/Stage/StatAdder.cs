3ae906fd48ad2ba42914d269b45133ad55807a00
cudema <54349175+cudema@users.noreply.github.com>
2025-12-04 22:24:39 +0900

    StatAdder

--- Assets/Scripts/Stage/StatAdder.cs
+++ Assets/Scripts/Stage/StatAdder.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,42 +8,71 @@ public enum Stat
     HP = 0,
     Defence,
     AttackDamage,
-    Speed,
     CriticalRange,
     CriticalDamage,
     Penetration
 }
 
+[Serializable]
+public struct AdderStatData
+{
+    [SerializeField]
+    Stat stat;
+    [SerializeField]
+    public Sprite icon;
+    [SerializeField]
+    public float value;
+}
+
 public class StatAdder : MonoBehaviour
 {
-    public void SetStat(Stat addStat)
+    [SerializeField]
+    AdderStatData[] adderStats;
+
+    public void AddStat(Stat addStat)
     {
         switch(addStat)
         {
             case Stat.HP:
-                Player.instance.HpBonus += 50;
+                Player.instance.HpBonus += adderStats[(int)Stat.HP].value;
                 break;
             case Stat.Defence:
-                Player.instance.DefenseBonus += 5;
+                Player.instance.DefenseBonus += adderStats[(int)Stat.Defence].value;
                 break;
             case Stat.AttackDamage:
-                Player.instance.AttackDamageBonus += 10;
-                break;
-            case Stat.Speed:
-                Player.instance.SpeedBonus += 1;
+                Player.instance.AttackDamageBonus += adderStats[(int)Stat.AttackDamage].value;
                 break;
             case Stat.CriticalRange:
-                Player.instance.CriticalRangeBonus += 0.05f;
+                Player.instance.CriticalRangeBonus += adderStats[(int)Stat.CriticalRange].value;
                 break;
             case Stat.CriticalDamage:
-                Player.instance.CriticalDamageBonus += 0.1f;
+                Player.instance.CriticalDamageBonus += adderStats[(int)Stat.CriticalDamage].value;
                 break;
             case Stat.Penetration:
-                Player.instance.PenetrationBonus += 5;
+                Player.instance.PenetrationBonus += adderStats[(int)Stat.Penetration].value;
                 break;
             default:
                 Debug.Log("Error");
                 break;
         }
+    }
+
+    void SetStat()
+    {
+        List<AdderStatData> tempList = new List<AdderStatData>(adderStats);
+
+        int index = UnityEngine.Random.Range(0, tempList.Count);
+        AdderStatData data0 = tempList[index];
+        tempList.RemoveAt(index);
+        
+        index = UnityEngine.Random.Range(0, tempList.Count);
+        AdderStatData data1 = tempList[index];
+        tempList.RemoveAt(index);
+
+        index = UnityEngine.Random.Range(0, tempList.Count);
+        AdderStatData data2 = tempList[index];
+        tempList.RemoveAt(index);
+
+        
     }
 }
