240364e95ce6b9384b28e5df77dc80fe8b5022ae
11by <jay3074115@gmail.com>
2026-04-03 22:30:38 +0900

    트레일 개선

--- Assets/Prefab/Effects/WeaponEffect.cs
+++ Assets/Prefab/Effects/WeaponEffect.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class WeaponEffect : MonoBehaviour
 {
     [SerializeField] private ParticleSystem weaponTrail;
-    [SerializeField] private ParticleSystem weaponTrail_2;
     public Animator animator;
 
     public void PlayTrail()
@@ -15,11 +14,8 @@ public class WeaponEffect : MonoBehaviour
         }
 
         weaponTrail.gameObject.SetActive(true);
-        weaponTrail.Clear();
+        
         weaponTrail.Play();
-        weaponTrail_2.gameObject.SetActive(true);
-        weaponTrail_2.Clear();
-        weaponTrail_2.Play();
     }
 
     public void StopTrail()
@@ -28,6 +24,5 @@ public class WeaponEffect : MonoBehaviour
             return;
 
         weaponTrail.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
-        weaponTrail_2.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
     }
 }
